59cd95834547179093f78b2f9f2f8b23a17390ba
Taylor Barnabic <tbarnabic1@gmail.com>
2021-11-05 12:13:55 -0400

    Created CREATE, DELETE routes for messages

---
+++ backend/routes/api/message.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { Op } = require('sequelize');
+
+const { Message } = require('../../db/models');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+router.get(
+	'/:channelId(\\d+)',
+	asyncHandler(async (req, res) => {
+		const { channelId } = req.params;
+		const messages = await Server.findAll({
+			where: {
+				channelId: channelId,
+			},
+		});
+		return res.json({ messages });
+	})
+);
+
+router.post(
+	'/',
+	asyncHandler(async (req, res) => {
+		const { message, userId, channelId } = req.body;
+		const message = await Message.create({
+			message,
+			userId,
+			channelId,
+		});
+		return res.json(message);
+	})
+);
+
+module.exports = router;

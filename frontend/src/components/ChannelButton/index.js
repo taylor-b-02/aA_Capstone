77dac943b67951639fa8c143fe0e58f3542db19f
Taylor Barnabic <tbarnabic1@gmail.com>
2022-11-19 21:56:26 -0500

    Created alternate class for focused channel button, focus state now on toggle

--- frontend/src/components/ChannelButton/index.js
+++ frontend/src/components/ChannelButton/index.js
@@ -1,37 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Modal from 'react-modal';
-import { FaCog, FaHashtag } from 'react-icons/fa';
+import { FaCog, FaHashtag } from "react-icons/fa";
 
-import css from './ChannelButton.module.css';
+import css from "./ChannelButton.module.css";
 
 function ChannelButton({ channel, setChannel, openModal }) {
+  const [isActive, setIsActive] = useState(false);
 
-    return (
-        <>
-            <div
-                key={channel.id}
-                value={channel.id}
-                className={css['channel']}
-                onClick={() => {
-                    console.log('CHANNEL ID', channel.id);
-                    setChannel(channel.id);
-                }}
-                tabIndex="0"
-            >
-                <FaHashtag className={css['channel-tag']} />
-                <div
-                    className={css['channel-name-container']}
-                    tabIndex="0"
-                >{`${channel.name}`}</div>
-                <FaCog
-                    onClick={openModal}
-                    value={channel.id}
-                    className={css['channel-settings']}
-                ></FaCog>
-            </div>
-        </>
-    );
+  const handleClick = () => {
+    console.log("CHANNEL ID", channel.id);
+    setChannel(channel.id);
+    
+    console.log("isActive: ", isActive);
+    setIsActive(!isActive);
+    console.log("isActive: ", isActive);
+  };
+
+  return (
+    <>
+      <div
+        key={channel.id}
+        value={channel.id}
+        className={isActive ? css["channel"] : css["channel-focused"]}
+        onClick={handleClick}
+        tabIndex="0"
+      >
+        <FaHashtag className={css["channel-tag"]} />
+        <div
+          className={css["channel-name-container"]}
+          tabIndex="0"
+        >{`${channel.name}`}</div>
+        <FaCog
+          onClick={openModal}
+          value={channel.id}
+          className={css["channel-settings"]}
+        ></FaCog>
+      </div>
+    </>
+  );
 }
 
 export default ChannelButton;

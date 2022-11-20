19e2a98b4810e25d3e31dd4787fdecf31f24a69c
Taylor Barnabic <tbarnabic1@gmail.com>
2022-11-20 11:24:49 -0500

    Removed unessary actions from the 'current' reducer

--- frontend/src/store/current.js
+++ frontend/src/store/current.js
@@ -1,8 +1,6 @@
 //~~~~~Actions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const SET_CURRENT_SERVER = 'current/setCurrentServer';
-const GET_CURRENT_SERVER = 'current/getCurrentServer';
 const SET_CURRENT_CHANNEL = 'current/setCurrentChannel';
-const GET_CURRENT_CHANNEL = 'current/getCurrentChannel';
 
 //~~~~~Action Creators~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -13,24 +11,10 @@ export const setServer = (server) => {
 	};
 };
 
-// !REMOVE - Unecessary, just use useSelector
-export const getServer = () => {
-	return {
-		type: GET_CURRENT_SERVER,
-	};
-};
-
 export const setChannel = (channel) => {
 	return {
 		type: SET_CURRENT_CHANNEL,
 		payload: channel,
-	};
-};
-
-// !REMOVE - Unecessary, just use useSelector
-export const getChannel = () => {
-	return {
-		type: GET_CURRENT_CHANNEL,
 	};
 };
 
@@ -43,18 +27,10 @@ const currentReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case SET_CURRENT_SERVER:
 			newState['server'] = action.payload;
-			console.log('SET CURR SERV');
 			return newState;
-		case GET_CURRENT_SERVER:
-			console.log('GET CURR SERVs');
-			return;
 		case SET_CURRENT_CHANNEL:
 			newState['channel'] = action.payload;
-			console.log('SET CURR CHANNEL');
 			return newState;
-		case GET_CURRENT_CHANNEL:
-			console.log('GET_CURR_CHANNEL');
-			return;
 		default:
 			return state;
 	}
